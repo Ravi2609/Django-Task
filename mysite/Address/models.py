f12558ae16cee0579c70584d3fdd7e88b8ba51e9
divyanshgupta01 <57209697+divyanshgupta01@users.noreply.github.com>
2019-10-31 12:01:28 +0530

    change field length of street address

--- mysite/Address/models.py
+++ mysite/Address/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Address(models.Model):
-    street_address=models.CharField(max_length=100)
+    street_address=models.CharField(max_length=150)
     city=models.CharField(max_length=50)
     state=models.CharField(max_length=50)
     pincode=models.IntegerField()
